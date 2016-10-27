33b8d749ad9599e6c20f7c644913e4e654fc05ee
Lucas Robertson <lucasrobertson0@gmail.com>
2016-10-27 16:39:38 -0700

    work

--- client/main.js
+++ client/main.js
@@ -1,6 +1,46 @@
 import {Template} from 'meteor/templating';
+import {Accounts} from 'meteor/accounts-base';
 
 import './main.html';
 
+Images = new FS.Collection("images", {
+    stores: [new FS.Store.FileSystem("images", {path: "~/uploads"})]
+});
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY',
+});
+Projects = new Meteor.Collection("projects");
 
-Template.projects.events({});
+Meteor.subscribe('projects');
+
+Template.projects.helpers({
+    projects() {
+        return Projects.find();
+    }
+});
+
+Template.projects.events({
+    'submit'(e) {
+        e.preventDefault();
+
+        if (!$('#addButton').hasClass('disabled')) {
+
+            var name = $('#nameTextBox').val();
+            var title = $('#titleTextBox').val();
+            var files = e.target.uploadInput.files;
+
+
+        }
+
+    },
+    'keyup'(e) {
+        var valid = $('#nameTextBox').val().length > 0
+            && $('#titleTextBox').val().length > 0;
+
+        if (valid) {
+            $('#addButton').removeClass('disabled');
+        } else {
+            $('#addButton').addClass('disabled');
+        }
+    }
+});
--- server/main.js
+++ server/main.js
@@ -1,1 +1,27 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
+
+Projects = new Mongo.Collection('projects');
+
+Images = new FS.Collection('images', {
+    stores: [new FS.Store.FileSystem('images')]
+});
+
+Meteor.publish('projects', function () {
+    return Projects.find();
+});
+
+Images.allow({
+    'insert': function () {
+        return true;
+    }
+});
+
+Meteor.methods({
+    'addProject'(name, title, imageUrl) {
+        Projects.insert({
+            name: name,
+            title: title,
+            imageUrl: imageUrl
+        });
+    }
+});
