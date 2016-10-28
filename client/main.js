29b65815babbe2b72626c9f36422fee1b3c47e61
Lucas Robertson <lucasrobertson0@gmail.com>
2016-10-27 17:21:22 -0700

    working

--- client/main.js
+++ client/main.js
@@ -3,8 +3,24 @@ import {Accounts} from 'meteor/accounts-base';
 
 import './main.html';
 
+Meteor.startup(function () {
+
+    sAlert.config({
+        effect: 'slide',
+        position: 'top-right',
+        timeout: 3000,
+        html: false,
+        onRouteClose: false,
+        stack: true,
+        offset: 0, // in px - will be added to first alert (bottom or top - depends of the position in config)
+        beep: false,
+        onClose: _.noop
+    });
+
+});
+
 Images = new FS.Collection("images", {
-    stores: [new FS.Store.FileSystem("images", {path: "~/uploads"})]
+    stores: [new FS.Store.FileSystem("images", '~/images')]
 });
 Accounts.ui.config({
     passwordSignupFields: 'USERNAME_ONLY',
@@ -12,10 +28,39 @@ Accounts.ui.config({
 Projects = new Meteor.Collection("projects");
 
 Meteor.subscribe('projects');
+Meteor.subscribe('images');
 
 Template.projects.helpers({
     projects() {
         return Projects.find();
+    },
+    images() {
+        return Images.find();
+    }
+});
+
+Template.project.helpers({
+    'name'() {
+        return Projects.findOne(this._id).name;
+    },
+    'title'() {
+        return Projects.findOne(this._id).title;
+    },
+    'image'() {
+        console.log(Images.find().fetch());
+        return Images.findOne(Projects.findOne(this._id).imageUrl);
+    }
+});
+
+Template.project.events({
+    'click #removeProjectButton'(event) {
+        Meteor.call('removeProject', this._id, function (err) {
+            if (err) {
+                sAlert.error(err.reason);
+            } else {
+                sAlert.success("Removed!");
+            }
+        });
     }
 });
 
@@ -28,6 +73,24 @@ Template.projects.events({
             var name = $('#nameTextBox').val();
             var title = $('#titleTextBox').val();
             var files = e.target.uploadInput.files;
+            var id;
+
+            for (var i = 0, ln = files.length; i < ln; i++) {
+                Images.insert(files[i], function (err, fileObj) {
+                    id = fileObj._id;
+                    console.log(id);
+                    Meteor.call('addProject', name, title, id, function (err) {
+                        if (err) {
+                            sAlert.error(err.reason);
+                        } else {
+                            sAlert.success("Added project!");
+                            $('#nameTextBox').val('');
+                            $('#titleTextBox').val('');
+                            $('#uploadInput').val('');
+                        }
+                    });
+                });
+            }
 
 
         }
--- server/main.js
+++ server/main.js
@@ -3,15 +3,28 @@ import {Meteor} from 'meteor/meteor';
 Projects = new Mongo.Collection('projects');
 
 Images = new FS.Collection('images', {
-    stores: [new FS.Store.FileSystem('images')]
+    stores: [new FS.Store.FileSystem('images', '~/images')]
 });
 
 Meteor.publish('projects', function () {
     return Projects.find();
 });
 
+Meteor.publish('images', function() {
+    return Images.find();
+});
+
 Images.allow({
     'insert': function () {
+        return true;
+    },
+    'update': function () {
+        return true;
+    },
+    'remove': function () {
+        return true;
+    },
+    'download': function() {
         return true;
     }
 });
@@ -23,5 +36,8 @@ Meteor.methods({
             title: title,
             imageUrl: imageUrl
         });
+    },
+    'removeProject'(id) {
+        Projects.remove(id);
     }
 });
